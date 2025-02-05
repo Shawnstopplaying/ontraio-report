4f3e55b18bcac2ba473c2833f2870e838c1cf004
Shawnstopplaying <q297591@gmail.com>
2025-02-05 16:30:34 -0500

    calc avg cells per envgroup

---
+++ code/data_analysis.R
@@ -0,0 +1,17 @@
+#data analysis
+
+#load packages
+library(tidyverse)
+
+#grab the data for our analysis
+sample_data <-read_csv("data/sample_data.csv")
+glimpse(sample_data)
+
+#sumarize
+summarize(sample_data, avg_cells= mean(cells_per_ml))
+
+#syntax/style
+sample_data %>%
+  #group the data by the envGroup
+  group_by(env_group) %>%
+  summarize(avg_cells = mean(cells_per_ml))
